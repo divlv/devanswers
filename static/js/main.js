32fbc9b3a75f41849a09fc79bbb483bb730c3eee
dima <dima@zaharov.info>
2023-05-17 15:02:23 +0300

    Adjusted clicks behavior; JSON minor fixes

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,23 @@
-const phraseText = document.getElementById("phrase-text");
-const phraseLink = document.getElementById("phrase-link");
-const logoLink = document.getElementById("logo-link");
-const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+    setRandomBackgroundColor();
+    handlePhraseClick();
+});
 
 function randomPastelColor() {
     const hue = Math.floor(Math.random() * 360);
     return `hsl(${hue}, 100%, 20%)`;
 }
 
-body.style.backgroundColor = randomPastelColor();
+function setRandomBackgroundColor() {
+    document.body.style.backgroundColor = randomPastelColor();
+}
 
-phraseLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.location.href = event.target.getAttribute("href");
-});
+function handlePhraseClick() {
+    const phraseText = document.getElementById("phrase-text");
+    const phraseLink = document.getElementById("phrase-link");
 
-logoLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    window.location.href = event.target.parentElement.getAttribute("href");
-});
+    phraseText.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = phraseLink.href;
+    });
+}
